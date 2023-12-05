70efa082680bf2845cf41ff5840237597a9929dc
Erhan Koral <erhan.16k@gmail.com>
2023-12-05 18:53:06 +0100

    break to day-1

--- Day-1/puzzle-1.js
+++ Day-1/puzzle-1.js
@@ -5,19 +5,53 @@ import { dirname, resolve } from 'path';
 const currentModulePath = fileURLToPath(import.meta.url);
 const absolutePath = resolve(dirname(currentModulePath), './puzzleInfo.txt');
 const fileContent = readFileSync(absolutePath, 'utf-8');
-const input = fileContent.split('\n');
+//const input = fileContent.split('\n');
+
+const input = ['two1nine',
+    'eightwothree',
+    'abcone2threexyz',
+    'xtwone3four',
+    '4nineeightseven2',
+    'zoneight234',
+    '7pqrstsixteen']
 
 const numbersArray = [];
 
+
+function convertItem(string) {
+    
+    const replacementObjects = {one:'1', two:'2', three:'3', four:'4', five:'5', six:'6', seven:'7', eight:'8', nine:'9'}
+
+    const arrayOfKeys = Object.keys(replacementObjects)
+    const arrayOfValues = Object.values(replacementObjects)
+    let index = 0;
+    for(let i in string) {
+        if(arrayOfValues.includes(string[i])){
+            index += 1;
+        }
+        else if (arrayOfKeys.includes(string.substring(index, i))){
+            let subString = string.substring(index, i);
+            string = string.replace(subString, replacementObjects[subString]);
+            index = 0
+            convertItem(string)
+        } 
+    }
+
+    // arrayOfStringNumbers.forEach(number => {
+    //    string = string.replace(number, replacementObjects[number])
+    // })
+    return string
+}
 
 
 
 
 input.map(item => {
+    const convertedItem = convertItem(item)
     let matches = []
-    for (let i in item){
-        if(item[i].match(/\d+/g)){
-            matches.push(item[i]);
+    for (let i in convertedItem){
+        if(convertedItem[i].match(/\d+/g)){
+            matches.push(convertedItem[i]);
         }
     }
     let newArr = []
@@ -34,5 +68,5 @@ input.map(item => {
  })
 
  const sumOfAll = numbersArray.reduce((acc, curr) => acc + curr)
-
+ console.log(numbersArray)
  console.log(sumOfAll)
